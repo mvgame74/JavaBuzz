4f85a8ea47274934ba1e8db7ca7efee289b7e0bd
Maria Valero Gonzalez <mariavalerogonzalez@ma376.home>
2021-06-01 15:25:58 +0100

    First Commit of JavaBuzz

---
+++ Week_6/JavaBuzz/src/Javabuzz.js
@@ -0,0 +1,11 @@
+class Javabuzz {
+  isDivisibleByThreeAndFive(number) {
+    return (number % 3 === 0 && number % 5 === 0 );
+  }
+  isDivisibleByThree(number) {
+    return (number % 3 === 0);
+  }
+  isDivisibleByFive(number) {
+    return (number % 5 === 0);
+  }
+}
